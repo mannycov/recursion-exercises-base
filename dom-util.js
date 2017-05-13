0049b5690e8f2b08ae662dee283f7c4f458dd54b
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2017-05-12 19:14:37 -0700

    added flattenTreeToArray function

--- dom-util.js
+++ dom-util.js
@@ -3,14 +3,16 @@ const visitAllNodes = function(node, callback) {
   callback(node);
   if (node.childNodes.length === 0) {
     return;
-  } else if (node.childNodes.length > 0) {
-      node.childNodes.forEach(childNode => visitAllNodes(childNode, callback));
-  } 
+  }
+    node.childNodes.forEach(childNode => visitAllNodes(childNode, callback)); 
 };
 
 const flattenTreeToArray = function(node) {
-  // Hint: Use visitAllNodes()
-  return visitAllNodes(node);
+  const arr = [];
+  visitAllNodes(node, function() {
+    arr.push(node)
+  });
+  return arr;
 };
 
 module.exports = {
